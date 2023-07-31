9f1d43b9c30813742ebba3c1698431ea294a7dae
Abe M <abe.malla8@gmail.com>
2023-07-30 22:45:18 -0700

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://abem.dev',
-	integrations: [mdx(), sitemap()],
+  site: 'https://abem.dev',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: cloudflare()
 });
