3d63c6f20a97d437d80360991ac2e844f0818302
Abe M <abe.malla8@gmail.com>
2023-07-31 15:34:13 -0700

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import mdx from '@astrojs/mdx';
+// import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abem.dev',
-  integrations: [mdx(), sitemap()],
-  output: 'server',
-  adapter: cloudflare()
+    site: 'https://abem.dev',
+    integrations: [sitemap()],
+    output: 'server',
+    adapter: cloudflare(),
 });
