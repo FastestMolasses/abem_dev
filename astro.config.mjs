856523b9bc525491395192ce5308d7c9d22c3a93
Abe M <abe.malla8@gmail.com>
2023-11-17 05:11:45 -0800

    Preparing update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
 // import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://abem.dev',
-    integrations: [sitemap()],
+    integrations: [
+        sitemap(),
+        react(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+    ],
     output: 'server',
     adapter: cloudflare(),
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/noop',
+        },
+    },
 });
